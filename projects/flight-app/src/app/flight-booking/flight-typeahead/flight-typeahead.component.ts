b527ba2d5c6449198390c24f0b4b6fa4768d6fe1
mikezks <michael.zikes@intauria.com>
2019-11-12 14:53:50 +0100

    RxJS

--- projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { timer, Observable, Subscription } from 'rxjs';
-import { tap, share, switchMap } from 'rxjs/operators';
-import { FlightService } from '@flight-workspace/flight-api';
+import { tap, share, switchMap, debounceTime, filter } from 'rxjs/operators';
+import { FlightService, Flight } from '@flight-workspace/flight-api';
+import { FormControl } from '@angular/forms';
+import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-typeahead',
@@ -13,10 +15,17 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   timer$: Observable<number>;
   timerSubscription: Subscription;
 
-  constructor(private flightService: FlightService) { }
+  control = new FormControl();
+  flights$: Observable<Flight[]>;
+  loading: boolean;
+
+  constructor(
+    private flightService: FlightService,
+    private http: HttpClient) { }
 
   ngOnInit() {
-    this.rxjsDemo();
+    //this.rxjsDemo();
+    this.initTypeahead();
   }
 
   rxjsDemo(): void {
@@ -38,7 +47,30 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
       .subscribe(console.log);
   }
 
+  initTypeahead(): void {
+    this.flights$ = this.control.valueChanges
+      .pipe(
+        debounceTime(300),
+        filter(from => from.length > 2),
+        tap(() => this.loading = true),
+        switchMap(from => this.load(from)),
+        tap(() => this.loading = false)
+      );
+  }
+
+  load(from: string): Observable<Flight[]>  {
+    const url = "http://www.angular.at/api/flight";
+
+    const params = new HttpParams()
+                        .set('from', from);
+
+    const headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
+  };
+
   ngOnDestroy(): void {
-    this.timerSubscription.unsubscribe();
+    //this.timerSubscription.unsubscribe();
   }
 }
